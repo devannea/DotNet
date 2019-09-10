d24ef1e0173f2336602c545b2399dcad6c985823
devannea <devannenotes@gmail.com>
2019-09-10 11:22:10 -0500

    Progress

--- ExerciseLogAPI2/CS321_W5D1_ExerciseLogAPI/ApiModels/LoginModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace CS321_W5D1_ExerciseLogAPI.ApiModels
-{
-    public class LoginModel
-    {
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
-}
